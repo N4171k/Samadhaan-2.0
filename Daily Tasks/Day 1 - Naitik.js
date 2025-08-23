4b02ee3d30f3b434c53aae9358251a2d9b112a10
Naitik Tiwari <144876588+N4171k@users.noreply.github.com>
2025-08-23 21:30:41 +0530

    Create Day 1 - Naitik.js

---
+++ Daily Tasks/Day 1 - Naitik.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Naitik",
+    age: 21,
+    major: "Computer Science",
+    id: "101"
+};
+  
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Major:", student.major);
+console.log("Student ID:", student.id);
