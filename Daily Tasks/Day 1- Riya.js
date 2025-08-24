eacae4cf517dd70e9010a7d83cdcde31b0eb75b2
Riya Gupta <145035831+riyaguptaaa@users.noreply.github.com>
2025-08-24 16:58:13 +0530

    Create Day 1- Riya.js

---
+++ Daily Tasks/Day 1- Riya.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Riya Gupta",
+    rollNo: 101,
+    course: "B.Tech CSE",
+    isActive: true
+  };
+  
+  console.log("Student Details:");
+  console.log("Name:", student.name);
+  console.log("Roll No:", student.rollNo);
+  console.log("Course:", student.course);
+  console.log("Active:", student.isActive ? "Yes" : "No");
