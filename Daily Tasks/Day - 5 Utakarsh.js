04ae8ac270724c4d67339fa18d0f698886ae8657
Utakarsh Singh Thakur <145524154+AgentUT1000@users.noreply.github.com>
2025-08-29 09:50:16 +0530

    Day - 5 Utakarsh.js

---
+++ Daily Tasks/Day - 5 Utakarsh.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const students = [
+    { id: 1, name: 'naitik', major: 'Computer Science' },
+    { id: 2, name: 'riya', major: 'Electrical Engineering' },
+    { id: 3, name: 'utakarsh', major: 'Mathematics' }
+];
+app.get('/', (req, res) => {
+    res.send('Welcome to the Student API!');
+});
+app.get('/api/students', (req, res) => {
+    res.json(students);
+});
+app.listen(port, () => {
+    console.log(Server is running at http://localhost:${port});
+});
