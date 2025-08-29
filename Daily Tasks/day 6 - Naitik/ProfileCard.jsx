f02ffda29a07f4ff3b82bdd40ce87067b6162ba5
Naitik Tiwari <144876588+N4171k@users.noreply.github.com>
2025-08-30 00:01:06 +0530

    Create ProfileCard.jsx

---
+++ Daily Tasks/day 6 - Naitik/ProfileCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ProfileCard.css'; 
+function ProfileCard({ imageUrl, name, title, bio }) {
+  return (
+
+    <div className="profile-card">
+      <img src={imageUrl} alt={`Profile of ${name}`} className="profile-image" />
+      <div className="profile-info">
+        <h2 className="profile-name">{name}</h2>
+        <p className="profile-title">{title}</p>
+        <p className="profile-bio">{bio}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
